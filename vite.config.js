7293ee93efaa70c3cbc3cf099e04bd6432c8c2ab
Akshay <akshay.patil@wisdomleaf.com>
2025-04-13 22:01:52 +0530

    `Update vite.config.js to enable terser for ES2021 syntax`

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Stopwatch-Timer-UIHackaton/', 
+  build: {
+    terserOptions: {
+      parse: {
+        // Enable terser to handle ES2021 syntax
+        ecma: 2020
+      }
+    }
+  }
 });
